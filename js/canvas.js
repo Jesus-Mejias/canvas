3a871854a72ffe2201268402e8dbe4e0210facb9
Jeix T <jeix.t01@gmail.com>
2019-06-20 16:04:24 -0500

    Funciones para dibujar

--- js/canvas.js
+++ js/canvas.js
@@ -1,1 +1,17 @@
 
+$(document).ready(function () {
+
+	var canvas = $("#skCanvas");
+	var ctx = canvas[0].getContext("2d");
+
+	$("#basic").on('click', function () {
+		basic(ctx);
+	})
+
+});
+
+function basic(ctx) {
+	ctx.beginPath();
+	ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+	ctx.stroke();
+}
