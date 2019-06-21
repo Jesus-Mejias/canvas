8065a49f8c728f0df1722b977b63df1c4a864c64
Jeix T <jeix.t01@gmail.com>
2019-06-20 19:28:36 -0500

    Primeros trazoz basicos

--- js/canvas.js
+++ js/canvas.js
@@ -1,17 +1,43 @@
 
 $(document).ready(function () {
 
+	// ]: Obtiene el elemento canvas
 	var canvas = $("#skCanvas");
+	// ]: Contexto para renderizado en dos dimenciones
 	var ctx = canvas[0].getContext("2d");
 
+	// ]: Boton para dibujos basicos
 	$("#basic").on('click', function () {
-		basic(ctx);
+
+		// ]: Borra todo el contenido del canvas
+		ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
+
+		// ]: Dibuja un rectangulo relleno
+		ctx.fillRect(25, 25, 40, 20);
+		// ]: Dibuja el contorno de un rectangulo
+		ctx.strokeRect(25, 50, 40, 20);
+
+		// |~> Trazos
+		// ]: Dibuja un circulo
+		ctx.beginPath();
+		ctx.arc(45, 90, 15, 0, 2 * Math.PI);
+		ctx.stroke();
+
+		// ]: Dibuja un triangulo
+		ctx.beginPath();
+		ctx.moveTo(30, 125);
+		ctx.lineTo(60, 140);
+		ctx.lineTo(60, 110);
+		ctx.closePath();
+		ctx.fill();
+
+	})
+
+	// ]: Boton para limpiar el canvas
+	$("#clear").on('click', function () {
+
+		// ]: Borra todo el contenido del canvas
+		ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
 	})
 
 });
-
-function basic(ctx) {
-	ctx.beginPath();
-	ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-	ctx.stroke();
-}
