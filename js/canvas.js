d344c3290e595d12fc7e943b274a737466f23db7
Jeix T <jeix.t01@gmail.com>
2019-06-21 16:02:41 -0500

    Creacion de botones

--- js/canvas.js
+++ js/canvas.js
@@ -1,10 +1,13 @@
-
+// (|:
 $(document).ready(function () {
 
 	// ]: Obtiene el elemento canvas
 	var canvas = $("#skCanvas");
 	// ]: Contexto para renderizado en dos dimenciones
 	var ctx = canvas[0].getContext("2d");
+	// ]: Puntos medios
+	var centerX = canvas[0].width / 2;
+	var centerY = canvas[0].height / 2;
 
 	// ]: Boton para dibujos basicos
 	$("#basic").on('click', function () {
@@ -12,22 +15,34 @@ $(document).ready(function () {
 		// ]: Borra todo el contenido del canvas
 		ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
 
+		// ]: Dibuja el titulo en el canvas
+		ctx.textAlign = "center";
+		ctx.font = "30pt Verdana";
+		ctx.fillText("Basic Drawing", centerX, 35);
+
+		// ]: Dibuja linea debajo del titulo
+		ctx.translate(0, 0.8);
+		ctx.beginPath();
+		ctx.moveTo(15, 45);
+		ctx.lineTo(815, 45);
+		ctx.stroke();
+
 		// ]: Dibuja un rectangulo relleno
-		ctx.fillRect(25, 25, 40, 20);
+		ctx.fillRect(55, 55, 80, 40);
 		// ]: Dibuja el contorno de un rectangulo
-		ctx.strokeRect(25, 50, 40, 20);
+		ctx.strokeRect(55, 105, 80, 40);
 
 		// |~> Trazos
 		// ]: Dibuja un circulo
 		ctx.beginPath();
-		ctx.arc(45, 90, 15, 0, 2 * Math.PI);
+		ctx.arc(95, 185, 30, 0, 2 * Math.PI);
 		ctx.stroke();
 
 		// ]: Dibuja un triangulo
 		ctx.beginPath();
-		ctx.moveTo(30, 125);
-		ctx.lineTo(60, 140);
-		ctx.lineTo(60, 110);
+		ctx.moveTo(65, 255);
+		ctx.lineTo(130, 270);
+		ctx.lineTo(130, 230);
 		ctx.closePath();
 		ctx.fill();
 
@@ -41,3 +56,4 @@ $(document).ready(function () {
 	})
 
 });
+// ]>~
