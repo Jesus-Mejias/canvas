58004334d7724f90ca879f68304aa2015112035a
Jeix T <jeix.t01@gmail.com>
2019-06-25 15:25:03 -0500

    Programar lapiz

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,8 @@
 // (|:
+// |~> Variables globales
+var movimientos = new Array(); // ]: Guarda la posicion del raton
+var pulsado;				   // ]: Estado del boton del raton	
+
 $(document).ready(function () {
 
 	// ]: Obtiene el elemento canvas
@@ -9,102 +13,16 @@ $(document).ready(function () {
 	var centerX = canvas[0].width / 2;
 	var centerY = canvas[0].height / 2;
 
-	// ]: Boton para dibujos basicos
+	// ]: Boton para dibujo basico
 	$("#basic").on('click', function () {
+		
+		$("skCanvas").mousedown(function (e) {
+			pulsado = true;
+			movimientos.push([e.pageX - this.offsetLeft, e.pageY - this.offsetTop, flase]);
+			repinta();
+		})
 
-		// ]: Borra todo el contenido del canvas
-		ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
-
-		// ]: Dibuja el titulo en el canvas
-		ctx.textAlign = "center";
-		ctx.font = "30pt Verdana";
-		ctx.fillText("Basic Drawing", centerX, 35);
-
-		// ]: Dibuja linea debajo del titulo
-		ctx.translate(0, 0.8);
-		ctx.beginPath();
-		ctx.moveTo(15, 45);
-		ctx.lineTo(735, 45);
-		ctx.stroke();
-
-		// ]: Dibuja un rectangulo relleno
-		ctx.fillRect(55, 55, 80, 40);
-		// ]: Dibuja el contorno de un rectangulo
-		ctx.strokeRect(55, 105, 80, 40);
-
-		// |~> Trazos
-		// ]: Dibuja un circulo
-		ctx.beginPath();
-		ctx.arc(95, 185, 30, 0, 2 * Math.PI);
-		ctx.stroke();
-
-		// ]: Dibuja un triangulo
-		ctx.beginPath();
-		ctx.moveTo(65, 255);
-		ctx.lineTo(130, 270);
-		ctx.lineTo(130, 230);
-		ctx.closePath();
-		ctx.fill();
-
-		// |~> Lineas
-		// ]: Continuas
-		ctx.lineWidth = 15;
-		ctx.beginPath();
-		ctx.lineCap = "butt";
-		ctx.moveTo(180, 85);
-		ctx.lineTo(380, 85);
-		ctx.stroke();
-
-		ctx.beginPath();
-		ctx.lineCap = "round";
-		ctx.moveTo(180, 110);
-		ctx.lineTo(380, 110);
-		ctx.stroke();
-
-		ctx.beginPath();
-		ctx.lineCap = "square";
-		ctx.moveTo(180, 135);
-		ctx.lineTo(380, 135);
-		ctx.stroke();
-
-		// ]: Discontiuas
-		ctx.lineWidth = 4
-		ctx.setLineDash([4, 14]);
-		ctx.beginPath();
-		ctx.lineCap = "square";
-		ctx.moveTo(180, 185);
-		ctx.lineTo(380, 185);
-		ctx.stroke();
-
-		ctx.setLineDash([4, 14, 18]);
-		ctx.beginPath();
-		ctx.lineCap = "square";
-		ctx.moveTo(180, 205);
-		ctx.lineTo(380, 205);
-		ctx.stroke();
-
-		ctx.setLineDash([8, 14, 1, 16]);
-		ctx.beginPath();
-		ctx.lineCap = "square";
-		ctx.moveTo(180, 225);
-		ctx.lineTo(380, 225);
-		ctx.stroke();
-
-		// |~> Espiral
-		a = 1;
-		b = 4;
-
-		ctx.setLineDash([]);
-		ctx.beginPath();
-		ctx.moveTo(550, centerY);
-		for (var i = 0; i < 230; i++) {
-			angulo = 0.1 * i;
-			x = 550 + (a + b * angulo) * Math.cos(angulo);
-			y = centerY + (a + b * angulo) * Math.sin(angulo);
-
-			ctx.lineTo(x, y);
-		}
-		ctx.stroke();
+		
 	})
 
 	// ]: Boton para limpiar el canvas
