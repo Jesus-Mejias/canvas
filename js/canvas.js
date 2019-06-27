967dcccabfb7e347e9e142bee931efbed2847bb5
Jeix T <jeix.t01@gmail.com>
2019-06-26 19:27:10 -0500

    Reacomodo de herramientas y dibujo basico inicial

--- js/canvas.js
+++ js/canvas.js
@@ -1,68 +1,68 @@
 // (|:
-// |~> Variables globales
-var movimientos = new Array(); // ]: Guarda la posicion del raton
-var pulsado;				   // ]: Estado del boton del raton	
-
 $(document).ready(function () {
 
 	// ]: Obtiene el elemento canvas
 	var canvas = $("#skCanvas");
 	// ]: ctxo para renderizado en dos dimenciones
 	var ctx = canvas[0].getContext("2d");
-	// ]: Puntos medios
-	var centerX = canvas[0].width / 2;
-	var centerY = canvas[0].height / 2;
+	// ]: Estado del boton del raton	
+	var pulsado;
+	// ]: Controla el color de la linea
+	var color = "#000000";
+	var size = 5;
+	// ]: Posicion del cursor
+	var x = 0;
+	var y = 0;
 
+	/* -------------------------------------------
+	*  |~> Acciones del raton
+	*  -------------------------------------------
+	*/ 
+
+	// ]: Boton del raton pulsado
 	$("#skCanvas").mousedown(function (e) {
 		pulsado = true;
-
-		movimientos.push([e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false]);
-		dibujar();
+		[x, y] = [e.offsetX, e.offsetY];
 	});
 
+	// ]: Movimiento del raton
 	$('#skCanvas').mousemove(function(e){
-        if(pulsado){
-            movimientos.push([e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true]);
-            dibujar();
-        }
+
+		if (pulsado) {
+			ctx.strokeStyle = color;
+			ctx.lineJoin = 'round';
+			ctx.lineCap = 'round';
+			ctx.lineWidth = size;
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(e.offsetX, e.offsetY);
+			ctx.stroke();
+			[x, y] = [e.offsetX, e.offsetY];
+		}
     });
- 
+ 	
+ 	// ]: Boton del raton sin pulsar
     $('#skCanvas').mouseup(function(e){
         pulsado = false;
     });
- 
+ 	
+ 	// ]: Raton fuera del canvas
     $('#skCanvas').mouseleave(function(e){
         pulsado = false;
     });
 
-    dibujar();
+    /* -------------------------------------------
+	*  |~> Botones que sirven como herramientas 
+	*  	   para interactuar con el canvas.	
+	*  -------------------------------------------
+	*/ 
 
+	// ]: Limpia lo que se dibuje en el canvas
     $("#clear").on('click', function () {
 
 		// ]: Borra todo el contenido del canvas
 		ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
 	});	
-
-	function dibujar() {
-
-		ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
-
-		ctx.lineJoin = "round";
-		ctx.lineWidth = 2;
-
-		for (var i = 0; i < movimientos.length; i++) {
-			ctx.beginPath();
-    		if(movimientos[i][2] && i){
-      			ctx.moveTo(movimientos[i-1][0], movimientos[i-1][1]);
-     		}else{
-      			ctx.moveTo(movimientos[i][0], movimientos[i][1]);
-     		}
-
-	     	ctx.lineTo(movimientos[i][0], movimientos[i][1]);
-	     	ctx.closePath();
-	     	ctx.stroke();
-		}
-	}
 
 });
 
